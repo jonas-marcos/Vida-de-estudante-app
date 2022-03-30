99ad2212796769540c593cdc076c577337352fc5
Aluno LabInfo <aluno.lab@ifsalinas.lan>
2022-03-30 19:26:05 -0300

    segundo commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default{
+    colors: {
+        primary: '#E5E5E5',
+
+        secondary: '#5CC6BA',
+
+        shape: '#323434',
+
+        title: '#A0A0A0',
+        background: '#FFFFFF',
+        text: '#717F7F', 
+
+        income: '#E56353',
+        expend: '#27A2DB',
+    }
+}
